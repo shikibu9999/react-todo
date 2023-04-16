2716008752f2bfea03a8e0483ebafaf2693983b5
shikibu9999 <diesel.only.the.diesel@gmail.com>
2023-04-16 19:23:45 +0900

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const todo = ({todo, toggleTodo}) => {
+
+  const handleTodoClick = () =>{
+    toggleTodo(todo.id);
+  };
+  return <div>
+    <label>
+      <input type="checkbox" checked={todo.completed} readOnly 
+      onChange={handleTodoClick}
+      />
+    </label>
+    {todo.name}
+    </div>;
+};
+
+export default todo;
